d535db993783d6f0ad9c1b1b2b43dfc826393095
mathmastin <math.mastin@gmail.com>
2015-05-30 15:16:36 -0400

    More renaming.

---
+++ src/NMS.py
@@ -0,0 +1,111 @@
+## Class definitions and methods for the NumString Python class
+__author__ = 'Matt Mastin'
+
+import itertools
+
+
+class NumString(object):
+    """NumString class
+
+    """
+
+    def __init__(self, digits=None):
+        """Constructor for NumString()
+
+        Generates a NumString from the given list.
+        A NumString is empty by default.
+
+        :type digits: list
+        """
+        self.digits = digits
+        self.total = sum(self.digits)
+        self.stringsize = len(self.digits)
+
+    def __str__(self):
+        """Return string type for NumString()
+
+        :rtype : string
+        """
+        if self.stringsize > 1:
+            outstring = ','.join(str(d) for d in self.digits)
+        else:
+            outstring = str(self.digits[0])
+
+        return outstring
+
+    def fixtotal(self):
+        """Call to reset sum to correct value"""
+        self.total = sum(self.digits)
+
+
+class NumStringPile(object):
+    """NumStringPile class
+
+    The NumStringPile is a list of all possible NumStrings
+    of a given Size
+    """
+
+    def __init__(self, stringsize=0):
+        """Constructor for NumStringPile()
+
+        Generates a list of all NumStrings of length Size
+        Size will default to 0 creating an empty Pile
+        This will build the pile in memory, so be warned
+        """
+        self.stringsize = stringsize
+        self.nums = itertools.product(range(0, self.stringsize), repeat=self.stringsize)
+        self.pile = []
+        for i in self.nums:
+            self.pile.append(NumString(i))
+
+    def __str__(self):
+        """Return string type for NumStringPile()
+
+        :rtype : string
+        """
+        if self.stringsize == 0:
+            outstring = 'Empty Pile'
+        else:
+            outstring = ';'.join(str(x) for x in self.pile)
+        return outstring
+
+
+class NSPIterator(object):
+    """Iterator for NumStringPile"""
+
+    def __init__(self, itpile=None):
+        self.itpile = itpile
+        if self.itpile is not None:
+            self.pilelength = len(self.itpile.pile)
+            self.step = self.pilelength
+        else:
+            self.step = 0
+
+    def next(self):
+        """Returns the next element in the pile"""
+        if self.step == 0:
+            raise StopIteration
+        self.step -= 1
+        return self.itpile.pile[(self.pilelength - 1) - self.step]
+
+    def __iter__(self):
+        """Returns the iterator"""
+        return self
+
+
+class NSPGenerator(object):
+    """Generator to create NumStringPiles"""
+
+    def __init__(self, stringsize=0):
+        self.stringsize = stringsize
+        self.sets = []
+        self.count = 0
+
+        for i in range(0, self.stringsize):
+            self.sets.append(range(0, self.stringsize))
+
+    def _getstring(self, index):
+        return self.sets[index]
+
+    def gen(self):
+        pass
---
+++ src/cartgen.py
@@ -0,0 +1,17 @@
+__author__ = 'Matt Mastin'
+
+
+def cart_prod(sets):
+    """Gives a generator for the cartesian product
+    of the lists in sets
+
+    :param sets: list of lists
+
+    :rtype : list
+    """
+    if not sets:
+        yield ()
+    else:
+        for i in sets[0]:
+            for x in cart_prod(sets[1:]):
+                yield (i,) + x
